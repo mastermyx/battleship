5a93602cf20a20c54329141d91daffdebcdd24c8
Arthur Hatchiguian <arthur.h@greenpandagames.com>
2018-05-25 17:08:37 +0200

    adding ship class. adding player class. adding ships assets

---
+++ battleship/Models/Player.swift
@@ -0,0 +1,13 @@
+//
+//  Player.swift
+//  battleship
+//
+//  Created by Arthur Hatchiguian on 25/05/2018.
+//  Copyright © 2018 Arthur Hatchiguian. All rights reserved.
+//
+
+import UIKit
+
+class Player: NSObject {
+
+}
